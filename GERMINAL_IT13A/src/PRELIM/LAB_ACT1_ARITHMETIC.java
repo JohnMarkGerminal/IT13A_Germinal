b90f66561455aaa65be7059af703eb4737bb5eca
Arriel Denver T. Cadavoz <jumjumescopete16@gmail.com>
2025-02-13 16:55:29 +0800

    "added Helloworld class"

---
+++ GERMINAL_IT13A/src/PRELIM/LAB_ACT1_ARITHMETIC.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package PRELIM;
+
+/**
+ *
+ * @author yielc
+ */
+public class LAB_ACT1_ARITHMETIC {
+    public static void main(String[] args){
+        int b = 10;
+        int c = 4;
+        int d = 6;
+        int a = 3;
+        
+        int xyz = b * c + d;
+        int jumjum = (b - c ) % d;
+        int abc = (b + c + d) / a;
+        int germinal = b * d - (c * c);
+        
+        System.out.println("10 * 4 + 6 = " + xyz);
+        System.out.println("10 - 4 % 6 = " + jumjum);
+        System.out.println("10 + 4 + 6 / 3 = " + abc);
+        System.out.println("10 * 6 - 4 * 4 = " + germinal);
+        
+        
+        
+      
+    }
+}

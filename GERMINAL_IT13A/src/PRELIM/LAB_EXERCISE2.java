956dc1bc296f5ac9835bffceeaa6b569531f75cd
JohnMarkGerminal <jumjumescopete16@gmail.com>
2025-02-24 10:27:24 +0800

    Add files via upload

---
+++ GERMINAL_IT13A/src/PRELIM/LAB_EXERCISE2.java
@@ -0,0 +1,31 @@
+
+
+package germinal_it13a;
+
+import java.util.Scanner; 
+
+
+
+public class LAB_EXERCISE2 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+         System.out.println("Enter the first number");
+         int num1 = input.nextInt();
+          System.out.println("Enter the second number");
+         int num2 = input.nextInt();
+         System.out.println("Enter the third number");
+         int num3 = input.nextInt();
+         
+         int greatest = num1;
+         
+         if (num2 > greatest) {
+             greatest = num2;
+          if (num3 > greatest) {   
+              greatest = num3;
+          }
+          System.out.println(" The greatest number is: " + greatest );
+          input.close();
+         }
+    }  
+    
+}

12bc95d79eaa950ab184b7cc950792205666322a
JohnMarkGerminal <jumjumescopete16@gmail.com>
2025-03-11 09:28:08 +0800

    Added Palindrome

---
+++ GERMINAL_IT13A/Palindrome.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Midterm;
+
+/**
+ *
+ * @author USER
+ */
+import java.util.Scanner;
+
+public class Palindrome {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a word or number: ");
+        String input = scanner.nextLine();
+        
+        String reverse = "";
+        
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reverse += input.charAt(i);
+        }
+        if (input.equalsIgnoreCase(reverse)) {
+            System.out.println("It is a Palindrome!");
+        } else {
+            System.out.println("It is not a Palindrome!");
+        }
+    }
+    
+}
